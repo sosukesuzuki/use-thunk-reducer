e4ed50509c1987bf25713b3ee81a465f6769c363
Sosuke Suzuki <aosukeke@gmail.com>
2019-02-25 19:14:06 +0900

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import typescript from "rollup-plugin-typescript2";
+import resolve from "rollup-plugin-node-resolve";
+import url from "rollup-plugin-url";
+import copy from "rollup-plugin-cpy";
+import external from "rollup-plugin-peer-deps-external";
+
+import pkg from "./package.json";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      exports: "named",
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: "es",
+      exports: "named",
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    typescript({
+      clean: true,
+      rollupCommonJSResolveHack: true,
+      exclude: ["*.d.ts", "**/*.d.ts"]
+    }),
+    resolve(),
+    url(),
+    external(),
+    copy([
+      {
+        files: ["src/index.ts"],
+        dest: "example/src/use-thunk-reducer"
+      }
+    ])
+  ]
+};
