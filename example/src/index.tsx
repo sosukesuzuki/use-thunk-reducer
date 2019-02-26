5e4999cf130c31205bf08fe137697fcf36660ca5
Sosuke Suzuki <aosukeke@gmail.com>
2019-02-27 00:15:32 +0900

    Fix example

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -3,53 +3,40 @@ import * as ReactDOM from "react-dom";
 import useThunkReducer from "./use-thunk-reducer";
 
 type State = { count: number };
-type Action = { type: "increment" | "decrement" | "reset" };
+type Action = { type: "increase"; amount: number };
 
 function reducer(state: State, action: Action): State {
   switch (action.type) {
-    case "increment":
-      return { count: state.count + 1 };
-    case "decrement":
-      return { count: state.count - 1 };
-    case "reset":
-      return { count: 0 };
+    case "increase":
+      return { count: state.count - action.amount };
     default:
       throw new Error();
   }
 }
 
-const initialState: State = { count: 0 };
-
-const incrementAsync = () => {
-  return (dispatch: React.Dispatch<Action>) => {
+const increaseAsync = (amount: number) => {
+  return (dispatch: React.Dispatch<Action>, state: State) => {
     setTimeout(() => {
-      dispatch({ type: "increment" });
+      console.log(state);
+      dispatch({ type: "increase", amount });
     }, 1000);
   };
 };
 
+const initialState: State = { count: 0 };
+
 function Counter() {
   const [state, dispatch] = useThunkReducer(reducer, initialState);
-  const increment = React.useCallback(
-    () => dispatch({ type: "increment" }),
+  const increaseTwoWithOneSecond = React.useCallback(
+    () => dispatch(increaseAsync(2)),
     []
   );
-  const incrementWithTime = React.useCallback(
-    () => dispatch(incrementAsync()),
-    []
-  );
-  const decrement = React.useCallback(
-    () => dispatch({ type: "decrement" }),
-    []
-  );
-  const reset = React.useCallback(() => dispatch({ type: "reset" }), []);
   return (
     <div>
       <p>{state.count}</p>
-      <button onClick={increment}>increment</button>
-      <button onClick={incrementWithTime}>increment with time</button>
-      <button onClick={decrement}>decrement</button>
-      <button onClick={reset}>reset</button>
+      <button onClick={increaseTwoWithOneSecond}>
+        increase 2 with 1 second
+      </button>
     </div>
   );
 }
